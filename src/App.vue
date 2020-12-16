9ea71783b6751c1f94b6697e1aa6c25d9b895206
Matti Tanskanen <mattitapanitanskanen@gmail.com>
2020-12-16 12:43:09 +0200

    Header as component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <Header />
+        <router-view/>
     </div>
-    <router-view/>
-  </div>
 </template>
+
+<script>
+import Header from './components/Header'
+export default {
+    name: 'App',
+    components: {
+        Header
+    }
+}
+</script>
 
 <style>
 #app {
