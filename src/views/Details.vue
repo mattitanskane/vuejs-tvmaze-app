3153ce32e3842e4c90480aaff65f7ccf76d0dd82
Matti Tanskanen <mattitapanitanskanen@gmail.com>
2020-12-16 17:22:25 +0200

    Lint

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -3,19 +3,20 @@
     <h1>{{ $route.params.name }}</h1>
     <p>S{{ $route.params.season }}E{{ $route.params.episode }}: Aired on {{ $route.params.airdate }} at {{ $route.params.airtime }}</p>
     <p>Runtime: {{ $route.params.runtime }} minutes</p>
-    <a :href="$route.params.more" target="_blank">More info at tvmaze.com</a>
+    <a
+      :href="$route.params.more"
+      target="_blank"
+    >
+      More info at tvmaze.com
+    </a>
     <h2>Episode summary</h2>
     <p v-html="$route.params.summary" />
-    {{$route.params.summary}}
   </article>
 </template>
 
 <script>
 // @ is an alias to /src
 export default {
-    name: 'Details',
-    methods: {
-
-    }
+    name: 'Details'
 }
 </script>
